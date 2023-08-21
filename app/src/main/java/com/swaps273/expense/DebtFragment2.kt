f7aea8008253c9c3d1ec0e7d3de2357cf9d36e4f
Swapnil-Srivastava273 <ss.bluescreenofd273@gmail.com>
2023-08-21 21:57:47 +0530

    Implemented minimization

--- app/src/main/java/com/swaps273/expense/DebtFragment2.kt
+++ app/src/main/java/com/swaps273/expense/DebtFragment2.kt
@@ -12,10 +12,22 @@ import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.TextView
 import com.swaps273.expense.placeholder.PlaceholderContent
+import java.lang.Double.min
+import java.util.*
+import kotlin.math.abs
 
 /**
  * A fragment representing a list of Items.
  */
+data class Pair2( var first : Double, var second : Int) : Comparable<Pair2>{
+    override fun compareTo(other: Pair2): Int {
+        if(this.first == other.first && this.second == other.second) return 0;
+        if(this.first > other.first) return 1;
+        if(this.first < other.first) return -1;
+        if(this.second > other.second) return 1;
+        return -1;
+    }
+};
 class DebtFragment2 : Fragment() {
 
     lateinit var debadapt: ArrayAdapter<String>
@@ -37,13 +49,41 @@ class DebtFragment2 : Fragment() {
         // Set the adapter
         var grp: ExpGroup= mainData.data[id1]
         listdeb= mutableListOf<String>()
-        for(i in 0..(mainData.data[id1].debt.size-1)){
-            for(j in 0..(mainData.data[id1].debt[i].size-1)){
-                if(mainData.data[id1].debt[i][j]<=-0.01){
-                    listdeb.add(mainData.data[id1].members[j].name+" -> "+mainData.data[id1].members[i].name+": "+(-mainData.data[id1].debt[i][j]).toString())
-                }
+        var diff = mutableListOf<Double>();
+        for(i in 0 .. (mainData.data[id1].debt.size) -1){
+            diff.add(0.0);
+        }
+        for(i in 0..(mainData.data[id1].debt.size-1)) {
+            for (j in 0..(mainData.data[id1].debt[i].size - 1)) {
+                diff[i] -= mainData.data[id1].debt[i][j];
+                diff[j] += mainData.data[id1].debt[i][j];
             }
         }
+        var values: SortedSet<Pair2> = TreeSet();
+        for(i in 0 .. (mainData.data[id1].debt.size -1)){
+            values.add(Pair2(diff[i]/2, i));
+        }
+        var eps: Double = 0.0000001;
+        while(values.size > 0){
+            var u = values.first();
+            var v = values.last();
+            values.remove(u);
+            values.remove(v);
+            val cost = min(-u.first, v.first);
+            if(cost < eps) continue;
+            listdeb.add(mainData.data[id1].members[u.second].name+" -> "+mainData.data[id1].members[v.second].name+": "+(cost).toString());
+            u = Pair2(u.first + cost, u.second);
+            v = Pair2(v.first - cost, v.second);
+            if(abs(u.first) > eps)values.add(u);
+            if(abs(v.first) > eps)values.add(v);
+        }
+//        for(i in 0..(mainData.data[id1].debt.size-1)){
+//            for(j in 0..(mainData.data[id1].debt[i].size-1)){
+//                if(mainData.data[id1].debt[i][j]<=-0.01){
+//                    listdeb.add(mainData.data[id1].members[j].name+" -> "+mainData.data[id1].members[i].name+": "+(-mainData.data[id1].debt[i][j]).toString())
+//                }
+//            }
+//        }
         var listvdeb: ListView =view.findViewById(R.id.list)
         debadapt= ArrayAdapter<String>(activity!!.applicationContext, android.R.layout.simple_list_item_1, listdeb)
         listvdeb.adapter=debadapt
@@ -54,12 +94,33 @@ class DebtFragment2 : Fragment() {
     fun refresh() {
         var grp: ExpGroup= mainData.data[id1]
         listdeb.clear()
-        for(i in 0..(mainData.data[id1].debt.size-1)){
-            for(j in 0..(mainData.data[id1].debt[i].size-1)){
-                if(mainData.data[id1].debt[i][j]<=-0.01){
-                    listdeb.add(mainData.data[id1].members[j].name+" -> "+mainData.data[id1].members[i].name+": "+(-mainData.data[id1].debt[i][j]).toString())
-                }
+        var diff = mutableListOf<Double>();
+        for(i in 0 .. (mainData.data[id1].debt.size) -1){
+            diff.add(0.0);
+        }
+        for(i in 0..(mainData.data[id1].debt.size-1)) {
+            for (j in 0..(mainData.data[id1].debt[i].size - 1)) {
+                diff[i] -= mainData.data[id1].debt[i][j];
+                diff[j] += mainData.data[id1].debt[i][j];
             }
+        }
+        var values: SortedSet<Pair2> = TreeSet();
+        for(i in 0 .. (mainData.data[id1].debt.size -1)){
+            values.add(Pair2(diff[i]/2, i));
+        }
+        var eps: Double = 0.0000001;
+        while(values.size > 0){
+            var u = values.first();
+            var v = values.last();
+            values.remove(u);
+            values.remove(v);
+            val cost = min(-u.first, v.first);
+            if(cost < eps) continue;
+            listdeb.add(mainData.data[id1].members[u.second].name+" -> "+mainData.data[id1].members[v.second].name+": "+(cost).toString());
+            u = Pair2(u.first + cost, u.second);
+            v = Pair2(v.first - cost, v.second);
+            if(abs(u.first) > eps)values.add(u);
+            if(abs(v.first) > eps)values.add(v);
         }
         debadapt.notifyDataSetChanged()
 
